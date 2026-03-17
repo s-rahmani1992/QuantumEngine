f431ec17e65ea0a15f2252dbcc3f706672d1970e
Sina Rahmani <rahmani.sina1992@gmail.com>
2026-03-17 00:10:58 +0000

    Fixed swap chain format compatibility with UNorm formats.

--- Engine/QuantumEngine.Rendering.Vulkan/Core/VulkanGraphicContext.cpp
+++ Engine/QuantumEngine.Rendering.Vulkan/Core/VulkanGraphicContext.cpp
@@ -110,7 +110,7 @@ bool QuantumEngine::Rendering::Vulkan::VulkanGraphicContext::InitializeSwapChain
 	vkGetPhysicalDeviceSurfaceFormatsKHR(m_physicalDevice, m_surface, &formatCount, formats.data());
 
 	for (const auto& availableFormat : formats) {
-		if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+		if (availableFormat.format == VK_FORMAT_R8G8B8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
 			m_swapChainFormat = availableFormat;
 			break;
 		}
